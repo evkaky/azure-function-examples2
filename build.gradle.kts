0a52c1c43041cca412a8ee150c9f2bac67c582c1
Mykola Ilminsky <Mykola.Ilminsky-ext@ab-Inbev.com>
2025-05-23 17:19:19 +0300

    Custom http traces attributes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    java
     id("com.microsoft.azure.azurefunctions") version "1.16.1"
 }
 
@@ -11,9 +11,10 @@ repositories {
 }
 
 dependencies {
-    implementation("ch.qos.logback:logback-classic:1.5.16")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.5.0")
+    implementation("org.springframework.boot:spring-boot-starter-actuator:3.5.0")
+    implementation("io.micrometer:micrometer-tracing-bridge-otel:1.5.0")
     implementation("com.microsoft.azure.functions:azure-functions-java-library:3.1.0")
-    implementation("com.azure:azure-messaging-servicebus:7.17.9")
 }
 
 java {
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,47 +1,59 @@
 package org.example;
 
-import com.azure.messaging.servicebus.ServiceBusReceivedMessage;
-import com.microsoft.azure.functions.*;
-import com.microsoft.azure.functions.annotation.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
+import io.micrometer.common.KeyValue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.observation.ObservationRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
 
 import java.util.Optional;
 
+import static com.microsoft.azure.functions.annotation.AuthorizationLevel.ANONYMOUS;
+
+@SpringBootApplication(exclude = GsonAutoConfiguration.class)
 public class Main {
-    private final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class);
+    }
+
+    @Bean
+    ObservationRegistryCustomizer<?> customCountryTraceAttribute() {
+        return registry -> registry.observationConfig()
+            .observationFilter((context) -> {
+                if (context instanceof ServerRequestObservationContext) {
+                    context.addLowCardinalityKeyValue(KeyValue.of("country", "GB"));
+                }
+                return context;
+            });
+    }
+}
+
+@Component
+class FunctionHandler {
+    @Autowired
+    RestClient.Builder builder;
 
     @FunctionName("HttpTriggerJava")
     public HttpResponseMessage run(
-        @HttpTrigger(name = "req", methods = HttpMethod.GET, authLevel = AuthorizationLevel.ANONYMOUS)
+        @HttpTrigger(name = "req", methods = HttpMethod.GET, authLevel = ANONYMOUS)
         HttpRequestMessage<Optional<String>> request
     ) {
-        // key=val pair is going to appear in "customDimensions" field in "traces" table in application insights
-        // for entry with message=test1
-        MDC.put("key", "val");
-        log.info("test1");
-        MDC.remove("key");
-
-        // but here
-        // key=val pair is NOT going to appear in "customDimensions" field in "traces" table in application insights
-        // for entry with message=test2
-        // I think this is wrong. Azure functions runtime probably should instrument addKeyValue() method as well
-        log.atInfo()
-            .addKeyValue("key", "val")
-            .log("test2");
-
-        return request.createResponseBuilder(HttpStatus.OK).body("response").build();
-    }
-
-    @FunctionName("MyServiceBusTrigger")
-    @FixedDelayRetry(maxRetryCount = 2, delayInterval = "00:00:10")
-    public void serviceBusTrigger(
-        @ServiceBusQueueTrigger(name = "message", queueName = "test-queue", connection = "AzureServiceBusConnection")
-        ServiceBusReceivedMessage message,
-        final ExecutionContext context
-    ) {
-        System.exit(1);
-        context.getLogger().info("Message Id: " + message.getMessageId());
+        RestClient restClient = builder.build();
+        String responseBody = restClient.get()
+            .uri("https://jsonplaceholder.typicode.com/todos/1")
+            .retrieve()
+            .body(String.class);
+        return request.createResponseBuilder(HttpStatus.OK).body(responseBody).build();
     }
 }
